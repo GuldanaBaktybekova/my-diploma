47ea1280a2115269d4dc271398ff7443b96b817a
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-23 22:26:18 +0600

    Delete 2img

--- src/components/MProductList/MProductItem/MProductItem.js
+++ src/components/MProductList/MProductItem/MProductItem.js
@@ -4,7 +4,6 @@ import classes from "./MProductItem.module.css";
 function MProductItem({ mproduct }) {
   return (
     <div className={classes.MProductItem}>
-      <img src={mproduct.image} alt="img" />
       <Link to={"/men/" + mproduct.mproductId}>{mproduct.title}</Link>
       <div className={classes.price}>${mproduct.price}</div>
     </div>
