6cf2a81d44e074d3f7beda9346553a2c001ce799
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-27 14:36:54 +0600

    Added header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header({ image, title, children }) {
   return (
     <header className="Header">
-      <img src={image} alt="img" />
+      <img src={image} alt="img" className="responsive"/>
       <section>
         <h1>{title}</h1>
         <p>{children}</p>
