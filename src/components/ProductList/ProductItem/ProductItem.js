b085328e902248941a35d2dce2b9a75216b05306
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-05-30 15:17:56 +0600

    Add new style

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -4,14 +4,11 @@ import classes from "./ProductItem.module.css";
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-      <Link to={"/products/" + product.productId}><img src={product.image} alt="img" /></Link>
-      <div className={classes.container}>
-        <Link to={"/products/" + product.productId}>{product.title} </Link>
-        <div className={classes.price}>${product.price}</div>
-       
-
-      </div>
-    </div>
+   
+    <Link to={"/products/" + product.productId}> <img src={product.image} alt={product.title} /> </Link>
+    <Link to={"/products/" + product.productId}>{product.title}</Link>
+    <div className={classes.price}>${product.price}</div>
+  </div>
   );
 }
 
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -10,6 +10,7 @@ function Contacts() {
         image={headerImage}>
          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, quasi nulla, harum asperiores excepturi dolor corrupti repellat sed odio consequatur dolorem! Reprehenderit quas mollitia neque quaerat dolorem accusamus vel nobis!
       </Header>
+  
     </>
   );
 }
