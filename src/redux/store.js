be950a0b4271552c96522df565b699bdb269aedf
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-24 15:27:59 +0600

    Add store.subscribe

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
-import { configureStore } from  '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
-  }
+  },
+});
+store.subscribe(() => {
+  localStorage.setItem(
+    "cartItems",
+    JSON.stringify(store.getState().cart.items)
+  );
 });
 
 export default store;
