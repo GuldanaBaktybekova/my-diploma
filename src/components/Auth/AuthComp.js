93dcfb4ada2f8acca24c193736ea4abe3662a820
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-24 14:39:55 +0600

    Add AuthComp component

---
+++ src/components/Auth/AuthComp.js
@@ -0,0 +1,20 @@
+import classes from "./AuthComp.module.css";
+function AuthComp() {
+  return ( 
+    <form className={classes.AuthComp}>
+    <label>
+        Email:
+        <input type="email" name="email" placeholder="email"/>
+    </label>
+    <label>
+        Password:
+        <input type="password" name="password" placeholder="password" />
+    </label>
+
+    <button>Sign in</button>
+    <button>Sign up</button>
+</form>
+   );
+}
+
+export default AuthComp;
