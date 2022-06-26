7a1a17ac2beea814685a46c1b5aae05a2768a5dd
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-26 18:06:04 +0600

    Add search input to the productList

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import ProductItem from "./ProductItem/ProductItem";
+
+
 import classes from "./ProductList.module.css";
 
 function ProductList({ products }) {
-  const productItems = products.map((product) => (
-    <ProductItem key={product.productId} product={product} />
-  ));
+  const [search, setSearch] = useState("");
+  const productItems = products
+    .filter((product) =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((product) => (
+      <ProductItem key={product.productId} product={product} />
+    ));
 
-  return <div className={classes.ProductList}>{productItems}</div>;
+  return (
+    <>
+     <input className={classes.search} type="text" value={search} onChange={({target})=> setSearch(target.value)} placeholder="Search" />
+      <div className={classes.ProductList}>{productItems}</div>
+    </>
+  );
 }
-
 export default ProductList;
