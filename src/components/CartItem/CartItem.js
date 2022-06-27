fa1b3b872a854eca3f399a251850208fdaea1480
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-27 12:12:38 +0600

    Add CartItem style and title

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -11,18 +11,36 @@ export default function CartItem({ product, actions, quantity }) {
       <div className={classes.image}>
         <img src={product.image} alt={product.title} />
       </div>
-      <div className={classes.title}>
-        <Link to="">{product.title}</Link>
-      </div>
-      <div className={classes.quantity}>
-        {actions ? <button onClick={() => dispatch(decrement(product.productId))}>-</button> : null}
-        {quantity}
-        {actions ? <button onClick={() => dispatch(increment(product.productId))}>+</button> : null}
-      </div>
-      <div className={classes.subtotal}>
-        ${product.price * quantity}
-        {actions ? <button onClick={() => dispatch(remove(product.productId))}>Delete</button> : null}
+      <div className={classes.text}>
+        <div className={classes.title}>
+          <Link to="">{product.title}</Link>
+        </div>
+        <div className={classes.quantity}>
+        Amount:
+          {actions ? (
+            <button onClick={() => dispatch(decrement(product.productId))}>
+              -
+            </button>
+          ) : null}
+          {quantity}
+          {actions ? (
+            <button onClick={() => dispatch(increment(product.productId))}>
+              +
+            </button>
+          ) : null}
+        </div>
+        <div className={classes.subtotal}>
+          Price:
+          ${product.price * quantity}
+          <br></br>
+          {actions ? (
+            <button className={classes.delete} onClick={() => dispatch(remove(product.productId))}>
+              Delete
+            </button>
+          ) : null}
+        </div>
       </div>
     </div>
-  )
-} 
+   
+  );
+}
