278385f34d44d01e9d19fc24da9dc30a933535b7
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-28 11:49:41 +0600

    Add key to authSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 import cartSlice from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    auth: authSlice,
   },
 });
 store.subscribe(() => {
@@ -13,4 +15,5 @@ store.subscribe(() => {
   );
 });
 
+console.log(store.getState().auth);
 export default store;
