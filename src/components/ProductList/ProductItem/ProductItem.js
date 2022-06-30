3054b75972d13f34b836628e913709643848dc79
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-06-30 12:14:20 +0600

    Add new product card style to ProductItem

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import classes from "./ProductItem.module.css";
 
+// import { useDispatch } from "react-redux";/
+
 function ProductItem({ product }) {
+  // const dispatch = useDispatch();
+  // function onAddToCart() {
+  //   dispatch(add(productId));
+  // }
   return (
     <div className={classes.ProductItem}>
       <Link to={"/products/" + product.productId}>
@@ -10,7 +16,7 @@ function ProductItem({ product }) {
       </Link>
       <Link to={"/products/" + product.productId}>{product.title}</Link>
       <div className={classes.price}>${product.price}</div>
-      {/* <div className={classes.buy}><button>{product.buy}</button></div> */}
+      {/* <button className={classes.cart} onClick={onAddToCart}>${product.cart}</button> */}
     </div>
   );
 }
